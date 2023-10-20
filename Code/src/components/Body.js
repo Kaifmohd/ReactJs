ffd04be1c8093f8d9567d9527da58f9c3b59a785
kaifmohd75 <kaifmohd75@gmail.com>
2023-10-20 16:09:28 +0530

    Deleted mock data and fetch the real data from Swiggy

--- Code/src/components/Body.js
+++ Code/src/components/Body.js
@@ -1,12 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
 
     //Local State variable - Super powerful variable
-    const [listOfRestaurants,setListOfRestaurant] = useState(resList);
+    const [listOfRestaurants,setListOfRestaurant] = useState([]);
 
+    useEffect(()=>{
+       fetchData();
+    },[]);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+            );
+        const json = await data.json();
+        console.log(json)
+        console.log(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
+        setListOfRestaurant(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
+    }
     return (
         <div className="body">
             <div className="filter">
